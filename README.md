f08a32245e48ed6fb6cdec3892b809059923ad17
Simona Debilio <simona.debilio@gmail.com>
2019-01-03 16:55:21 +0100

    Add for loop

---
+++ README.md
@@ -0,0 +1,4 @@
+# Pippo
+
+Questo e' un file di prova per apprendere l'uso di `Git`
+
---
+++ pippo.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main() {
+  int i;
+  for(i = 0; i < 10; i+=2) {
+    printf("Conto %d", i);
+  }
+
+  return 0;
+}
+
